3f252e3ed6056cd2b5e42e26df4fe448c9aa61d5
Portable-dev <oleeefr@yahoo.Fr>
2022-06-10 09:27:00 +0200

    maj des tags en fonction de la recherche generale

--- Scripts/Library/Render/SelectRecettes.js
+++ Scripts/Library/Render/SelectRecettes.js
@@ -1,13 +1,18 @@
 import {SearchRecipesRequest} from '../Requests/SearchRecipesRequest.js'
 import {Recettes} from '../Render/Recettes.js'
+import { getAllTags } from '../Requests/getAllTags.js';
+import { Tags } from '../Render/Tags.js';
 
 export class SelectRecettes {
 
     static by (character, listRecette) {
         let recettes = new SearchRecipesRequest (character, listRecette);
-        let result = recettes.getResultRecipe ();
+        let resultRecette = recettes.getResultRecipe ();
         //console.log(result);
-        Recettes.Display(result);
+        let resultTags = new getAllTags(resultRecette);
+        //console.log(resultTags);
+        Tags.Display(resultTags);
+        Recettes.Display(resultRecette);
     }
 }
 
